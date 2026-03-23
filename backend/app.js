bdccd135b3857d64e0041ce2dda9ea66afd4bc19
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2026-03-24 01:01:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,10 @@ const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: [
+    "http://localhost:5173",
+    "https://hd-notes-frontend.onrender.com"
+  ], credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
