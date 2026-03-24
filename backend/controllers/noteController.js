cf5770b80c5f149eaee4ac7f3e689969269ea77f
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2026-03-24 20:09:59 +0530

    Update noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,4 +1,5 @@
 const Note = require("../models/note");
+const CryptoJS = require("crypto-js");
 
 module.exports.getNotes = async(req, res) => {
 
@@ -8,13 +9,23 @@ module.exports.getNotes = async(req, res) => {
     } else {
 
         const notes = await Note.find({ userId: req.userId });
-        return res.json({ message: "All notes", notes: notes });
+        const decryptNote=notes.map((note)=>{
+
+            const bytes = CryptoJS.AES.decrypt(note.text, process.env.CRYPTO_SECRET);
+            const originalText = bytes.toString(CryptoJS.enc.Utf8);
+            note.text=originalText;
+            return note;
+        });
+
+        return res.json({ message: "All notes", notes: decryptNote });
     }
 };
 
 module.exports.createNote = async(req, res) => {
 
     const { newNote } = req.body;
+    const ciphertext = CryptoJS.AES.encrypt(newNote.text, process.env.CRYPTO_SECRET).toString();
+    newNote.text=ciphertext;
     const note = new Note({...newNote, userId: req.userId });
     await note.save();
     return res.json({ message: "Notes add Successfully" });
